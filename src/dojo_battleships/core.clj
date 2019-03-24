7aa3e172fc64fbb0135f4c74504b6abc226643fa
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-03-24 16:30:57 +0300

    initial state

--- src/dojo_battleships/core.clj
+++ src/dojo_battleships/core.clj
@@ -1,9 +1,16 @@
 (ns dojo-battleships.core
-    (:require [clojure2d.core :refer :all])
-    (:gen-class))
+  (:require [clojure2d.core :refer :all])
+  (:gen-class))
 
+(def field-size 10)
+(def ships [5 4 4 3 3 2 2 2 1 1 1 1])
 (def canvas-width  800)
 (def canvas-height 600)
+
+
+(defn render-board [board] nil)
+
+(defn handle-click [event state] state)
 
 (defn draw
   "Some function decription."
@@ -11,17 +18,20 @@
    window ;; window bound to function (for mouse movements)
    ^long framecount ;; frame number
    state] ;; state (if any)
-  () ; should return a new state
+  nil ; should return a new state
   )
 
 ;; create canvas, display window and draw on canvas via draw function (60 fps)
 ;; show-window {:keys [canvas window-name w h fps draw-fn state draw-state setup hint refresher always-on-top? background]
-(def window (show-window {:canvas (canvas canvas-width canvas-height),
-                          :window-name "Battleships",
-                          :fps 25,
-                          :draw-fn draw,
-                          :setup (fn [canvas window] ())}) ; returns initial state
-  )
+(def window (show-window {:canvas (canvas canvas-width canvas-height)
+                          :window-name "Battleships"
+                          :fps 25
+                          :draw-fn draw
+                          :setup (fn [canvas window]
+                                   {:state :setup ; :game :game-over
+                                    :setup-ships-left ships
+                                    :current-player :player-1
+                                    :board [{:x 0 :y 0 :hit? false :ship? false}]})}))
 
 (defn main []
   window)
