beb0d0db472638c6d502d40ffb329defe86177dd
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-03-24 15:51:07 +0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dojo-battleships
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/dojo_battleships/core.clj
@@ -0,0 +1,7 @@
+(ns dojo-battleships.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
