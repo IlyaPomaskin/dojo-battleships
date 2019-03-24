faccd7a91bd64102bdd9eea414b86887205aab93
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-03-24 18:41:23 +0300

    hit

--- src/dojo_battleships/core.clj
+++ src/dojo_battleships/core.clj
@@ -53,18 +53,50 @@
             (render-fn canvas cell)))
         board))
 
+(defn can-be-hit? [[x y] cell]
+  (and (= (:x cell) x)
+       (= (:y cell) y)
+       (false? (:hit? cell))))
+
+(defn hit-cell [coords state]
+  (update-in
+   state
+   [(:current-player state) :board]
+   (fn [board]
+     (mapv
+      #(if (can-be-hit? coords %1)
+         (assoc %1 :hit? true)
+         %1)
+      board))))
+
+(defn switch-player [state]
+  (update-in
+   state
+   [:current-player]
+   #(if (= %1 :player-1)
+      :player-2
+      :player-1)))
+
+(defn hit [coords state]
+  (->> state
+       (hit-cell coords)
+       (switch-player)))
+
 (defmethod mouse-event [window-name :mouse-pressed] [event state]
   ;; do something when button is pressed
-  (println (.-x (.getPoint event)))
-  state)
+  (let [x (quot (.-x (.getPoint event)) field-size)
+        y (quot (.-y (.getPoint event)) field-size)]
+    (println "x:" x "y:" y)
+    (hit [x y] state)))
 
 (defn draw
   [canvas
    window
    ^long framecount
    state]
-  (set-background canvas :lightblue 200)
-  (render-board canvas (get-in state [(:current-player state) :board]))
+  (let [global-state (get-state window)]
+    (set-background canvas :lightblue 200)
+    (render-board canvas (get-in global-state [(:current-player global-state) :board])))
   state)
 
 (defn init-board [size]
@@ -74,16 +106,43 @@
 
 (def window (show-window {:canvas (canvas canvas-width canvas-height)
                           :window-name window-name
-                          :fps 25
+                          :fps 1
                           :draw-fn draw
-                          :setup (fn [canvas window]
-                                   {:state :setup ; :game :game-over
-                                    :current-player :player-1
-                                    :player-1 {:setup-ships-left ships
-                                               :board (init-board world-size)}
-                                    :player-2 {:setup-ships-left ships
-                                               :board (init-board world-size)}})}))
+                          :state {:state :game
+                                  :current-player :player-1
+                                  :player-1 {:setup-ships-left ships
+                                             :board [{:x 0 :y 0 :hit? false :ship? true}
+                                                     {:x 0 :y 1 :hit? false :ship? true}
+                                                     {:x 0 :y 2 :hit? false :ship? true}
+                                                     {:x 0 :y 3 :hit? false :ship? false}
+                                                     {:x 1 :y 0 :hit? false :ship? false}
+                                                     {:x 1 :y 1 :hit? false :ship? false}
+                                                     {:x 1 :y 2 :hit? false :ship? false}
+                                                     {:x 1 :y 3 :hit? false :ship? false}
+                                                     {:x 2 :y 0 :hit? false :ship? false}
+                                                     {:x 2 :y 1 :hit? false :ship? false}
+                                                     {:x 2 :y 2 :hit? false :ship? false}
+                                                     {:x 2 :y 3 :hit? false :ship? false}
+                                                     {:x 3 :y 0 :hit? false :ship? false}
+                                                     {:x 3 :y 1 :hit? false :ship? false}
+                                                     {:x 3 :y 2 :hit? false :ship? false}
+                                                     {:x 3 :y 3 :hit? false :ship? false}]}
+                                  :player-2 {:setup-ships-left ships
+                                             :board [{:x 0 :y 0 :hit? false :ship? false}
+                                                     {:x 0 :y 1 :hit? false :ship? false}
+                                                     {:x 0 :y 2 :hit? false :ship? false}
+                                                     {:x 0 :y 3 :hit? false :ship? false}
+                                                     {:x 1 :y 0 :hit? false :ship? false}
+                                                     {:x 1 :y 1 :hit? false :ship? false}
+                                                     {:x 1 :y 2 :hit? false :ship? false}
+                                                     {:x 1 :y 3 :hit? false :ship? false}
+                                                     {:x 2 :y 0 :hit? false :ship? false}
+                                                     {:x 2 :y 1 :hit? false :ship? false}
+                                                     {:x 2 :y 2 :hit? false :ship? false}
+                                                     {:x 2 :y 3 :hit? false :ship? false}
+                                                     {:x 3 :y 0 :hit? false :ship? true}
+                                                     {:x 3 :y 1 :hit? false :ship? true}
+                                                     {:x 3 :y 2 :hit? false :ship? true}
+                                                     {:x 3 :y 3 :hit? false :ship? false}]}}}))
 
-(defn main []
-  window)
-
+(defn main [] window)
