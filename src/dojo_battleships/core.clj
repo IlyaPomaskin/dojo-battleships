a51fbbd5eee5626e3e5f1526db292b3c7c588264
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-03-24 19:02:12 +0300

    big field

--- src/dojo_battleships/core.clj
+++ src/dojo_battleships/core.clj
@@ -133,35 +133,221 @@
                                              :board [{:x 0 :y 0 :hit? false :ship? true}
                                                      {:x 0 :y 1 :hit? false :ship? true}
                                                      {:x 0 :y 2 :hit? false :ship? true}
-                                                     {:x 0 :y 3 :hit? false :ship? false}
+                                                     {:x 0 :y 3 :hit? false :ship? true}
+                                                     {:x 0 :y 4 :hit? false :ship? false}
+                                                     {:x 0 :y 5 :hit? false :ship? false}
+                                                     {:x 0 :y 6 :hit? false :ship? false}
+                                                     {:x 0 :y 7 :hit? false :ship? false}
+                                                     {:x 0 :y 8 :hit? false :ship? false}
+                                                     {:x 0 :y 9 :hit? false :ship? false}
+
                                                      {:x 1 :y 0 :hit? false :ship? false}
                                                      {:x 1 :y 1 :hit? false :ship? false}
                                                      {:x 1 :y 2 :hit? false :ship? false}
                                                      {:x 1 :y 3 :hit? false :ship? false}
-                                                     {:x 2 :y 0 :hit? false :ship? false}
+                                                     {:x 1 :y 4 :hit? false :ship? false}
+                                                     {:x 1 :y 5 :hit? false :ship? false}
+                                                     {:x 1 :y 6 :hit? false :ship? false}
+                                                     {:x 1 :y 7 :hit? false :ship? false}
+                                                     {:x 1 :y 8 :hit? false :ship? false}
+                                                     {:x 1 :y 9 :hit? false :ship? false}
+
+                                                     {:x 2 :y 0 :hit? false :ship? true}
                                                      {:x 2 :y 1 :hit? false :ship? false}
                                                      {:x 2 :y 2 :hit? false :ship? false}
                                                      {:x 2 :y 3 :hit? false :ship? false}
-                                                     {:x 3 :y 0 :hit? false :ship? false}
+                                                     {:x 2 :y 4 :hit? false :ship? false}
+                                                     {:x 2 :y 5 :hit? false :ship? false}
+                                                     {:x 2 :y 6 :hit? false :ship? false}
+                                                     {:x 2 :y 7 :hit? false :ship? false}
+                                                     {:x 2 :y 8 :hit? false :ship? false}
+                                                     {:x 2 :y 9 :hit? false :ship? false}
+
+                                                     {:x 3 :y 0 :hit? false :ship? true}
                                                      {:x 3 :y 1 :hit? false :ship? false}
                                                      {:x 3 :y 2 :hit? false :ship? false}
-                                                     {:x 3 :y 3 :hit? false :ship? false}]}
+                                                     {:x 3 :y 3 :hit? false :ship? false}
+                                                     {:x 3 :y 4 :hit? false :ship? false}
+                                                     {:x 3 :y 5 :hit? false :ship? false}
+                                                     {:x 3 :y 6 :hit? false :ship? false}
+                                                     {:x 3 :y 7 :hit? false :ship? false}
+                                                     {:x 3 :y 8 :hit? false :ship? false}
+                                                     {:x 3 :y 9 :hit? false :ship? false}
+
+                                                     {:x 4 :y 0 :hit? false :ship? true}
+                                                     {:x 4 :y 1 :hit? false :ship? false}
+                                                     {:x 4 :y 2 :hit? false :ship? false}
+                                                     {:x 4 :y 3 :hit? false :ship? false}
+                                                     {:x 4 :y 4 :hit? false :ship? false}
+                                                     {:x 4 :y 5 :hit? false :ship? false}
+                                                     {:x 4 :y 6 :hit? false :ship? false}
+                                                     {:x 4 :y 7 :hit? false :ship? false}
+                                                     {:x 4 :y 8 :hit? false :ship? false}
+                                                     {:x 4 :y 9 :hit? false :ship? false}
+
+                                                     {:x 5 :y 0 :hit? false :ship? true}
+                                                     {:x 5 :y 1 :hit? false :ship? false}
+                                                     {:x 5 :y 2 :hit? false :ship? false}
+                                                     {:x 5 :y 3 :hit? false :ship? false}
+                                                     {:x 5 :y 4 :hit? false :ship? false}
+                                                     {:x 5 :y 5 :hit? false :ship? false}
+                                                     {:x 5 :y 6 :hit? false :ship? false}
+                                                     {:x 5 :y 7 :hit? false :ship? false}
+                                                     {:x 5 :y 8 :hit? false :ship? false}
+                                                     {:x 5 :y 9 :hit? false :ship? false}
+
+                                                     {:x 6 :y 0 :hit? false :ship? false}
+                                                     {:x 6 :y 1 :hit? false :ship? false}
+                                                     {:x 6 :y 2 :hit? false :ship? false}
+                                                     {:x 6 :y 3 :hit? false :ship? false}
+                                                     {:x 6 :y 4 :hit? false :ship? false}
+                                                     {:x 6 :y 5 :hit? false :ship? false}
+                                                     {:x 6 :y 6 :hit? false :ship? false}
+                                                     {:x 6 :y 7 :hit? false :ship? false}
+                                                     {:x 6 :y 8 :hit? false :ship? false}
+                                                     {:x 6 :y 9 :hit? false :ship? false}
+
+                                                     {:x 7 :y 0 :hit? false :ship? false}
+                                                     {:x 7 :y 1 :hit? false :ship? false}
+                                                     {:x 7 :y 2 :hit? false :ship? false}
+                                                     {:x 7 :y 3 :hit? false :ship? false}
+                                                     {:x 7 :y 4 :hit? false :ship? false}
+                                                     {:x 7 :y 5 :hit? false :ship? false}
+                                                     {:x 7 :y 6 :hit? false :ship? false}
+                                                     {:x 7 :y 7 :hit? false :ship? false}
+                                                     {:x 7 :y 8 :hit? false :ship? false}
+                                                     {:x 7 :y 9 :hit? false :ship? false}
+
+                                                     {:x 8 :y 0 :hit? false :ship? false}
+                                                     {:x 8 :y 1 :hit? false :ship? false}
+                                                     {:x 8 :y 2 :hit? false :ship? false}
+                                                     {:x 8 :y 3 :hit? false :ship? false}
+                                                     {:x 8 :y 4 :hit? false :ship? false}
+                                                     {:x 8 :y 5 :hit? false :ship? false}
+                                                     {:x 8 :y 6 :hit? false :ship? false}
+                                                     {:x 8 :y 7 :hit? false :ship? false}
+                                                     {:x 8 :y 8 :hit? false :ship? false}
+                                                     {:x 8 :y 9 :hit? false :ship? false}
+
+                                                     {:x 9 :y 0 :hit? false :ship? false}
+                                                     {:x 9 :y 1 :hit? false :ship? false}
+                                                     {:x 9 :y 2 :hit? false :ship? false}
+                                                     {:x 9 :y 3 :hit? false :ship? false}
+                                                     {:x 9 :y 4 :hit? false :ship? false}
+                                                     {:x 9 :y 5 :hit? false :ship? false}
+                                                     {:x 9 :y 6 :hit? false :ship? false}
+                                                     {:x 9 :y 7 :hit? false :ship? false}
+                                                     {:x 9 :y 8 :hit? false :ship? false}
+                                                     {:x 9 :y 9 :hit? false :ship? false}]}
                                   :player-2 {:setup-ships-left ships
                                              :board [{:x 0 :y 0 :hit? false :ship? false}
                                                      {:x 0 :y 1 :hit? false :ship? false}
                                                      {:x 0 :y 2 :hit? false :ship? false}
                                                      {:x 0 :y 3 :hit? false :ship? false}
+                                                     {:x 0 :y 4 :hit? false :ship? false}
+                                                     {:x 0 :y 5 :hit? false :ship? false}
+                                                     {:x 0 :y 6 :hit? false :ship? false}
+                                                     {:x 0 :y 7 :hit? false :ship? false}
+                                                     {:x 0 :y 8 :hit? false :ship? false}
+                                                     {:x 0 :y 9 :hit? false :ship? false}
+
                                                      {:x 1 :y 0 :hit? false :ship? false}
-                                                     {:x 1 :y 1 :hit? false :ship? false}
+                                                     {:x 1 :y 1 :hit? false :ship? true}
                                                      {:x 1 :y 2 :hit? false :ship? false}
                                                      {:x 1 :y 3 :hit? false :ship? false}
+                                                     {:x 1 :y 4 :hit? false :ship? false}
+                                                     {:x 1 :y 5 :hit? false :ship? false}
+                                                     {:x 1 :y 6 :hit? false :ship? false}
+                                                     {:x 1 :y 7 :hit? false :ship? false}
+                                                     {:x 1 :y 8 :hit? false :ship? false}
+                                                     {:x 1 :y 9 :hit? false :ship? false}
+
                                                      {:x 2 :y 0 :hit? false :ship? false}
                                                      {:x 2 :y 1 :hit? false :ship? false}
                                                      {:x 2 :y 2 :hit? false :ship? false}
-                                                     {:x 2 :y 3 :hit? false :ship? false}
-                                                     {:x 3 :y 0 :hit? false :ship? true}
-                                                     {:x 3 :y 1 :hit? false :ship? true}
-                                                     {:x 3 :y 2 :hit? false :ship? true}
-                                                     {:x 3 :y 3 :hit? false :ship? false}]}}}))
+                                                     {:x 2 :y 3 :hit? false :ship? true}
+                                                     {:x 2 :y 4 :hit? false :ship? false}
+                                                     {:x 2 :y 5 :hit? false :ship? false}
+                                                     {:x 2 :y 6 :hit? false :ship? false}
+                                                     {:x 2 :y 7 :hit? false :ship? false}
+                                                     {:x 2 :y 8 :hit? false :ship? false}
+                                                     {:x 2 :y 9 :hit? false :ship? false}
+
+                                                     {:x 3 :y 0 :hit? false :ship? false}
+                                                     {:x 3 :y 1 :hit? false :ship? false}
+                                                     {:x 3 :y 2 :hit? false :ship? false}
+                                                     {:x 3 :y 3 :hit? false :ship? false}
+                                                     {:x 3 :y 4 :hit? false :ship? false}
+                                                     {:x 3 :y 5 :hit? false :ship? false}
+                                                     {:x 3 :y 6 :hit? false :ship? false}
+                                                     {:x 3 :y 7 :hit? false :ship? false}
+                                                     {:x 3 :y 8 :hit? false :ship? false}
+                                                     {:x 3 :y 9 :hit? false :ship? false}
+
+                                                     {:x 4 :y 0 :hit? false :ship? false}
+                                                     {:x 4 :y 1 :hit? false :ship? false}
+                                                     {:x 4 :y 2 :hit? false :ship? false}
+                                                     {:x 4 :y 3 :hit? false :ship? false}
+                                                     {:x 4 :y 4 :hit? false :ship? false}
+                                                     {:x 4 :y 5 :hit? false :ship? false}
+                                                     {:x 4 :y 6 :hit? false :ship? false}
+                                                     {:x 4 :y 7 :hit? false :ship? false}
+                                                     {:x 4 :y 8 :hit? false :ship? false}
+                                                     {:x 4 :y 9 :hit? false :ship? false}
+
+                                                     {:x 5 :y 0 :hit? false :ship? false}
+                                                     {:x 5 :y 1 :hit? false :ship? false}
+                                                     {:x 5 :y 2 :hit? false :ship? false}
+                                                     {:x 5 :y 3 :hit? false :ship? false}
+                                                     {:x 5 :y 4 :hit? false :ship? false}
+                                                     {:x 5 :y 5 :hit? false :ship? false}
+                                                     {:x 5 :y 6 :hit? false :ship? false}
+                                                     {:x 5 :y 7 :hit? false :ship? false}
+                                                     {:x 5 :y 8 :hit? false :ship? false}
+                                                     {:x 5 :y 9 :hit? false :ship? true}
+
+                                                     {:x 6 :y 0 :hit? false :ship? false}
+                                                     {:x 6 :y 1 :hit? false :ship? false}
+                                                     {:x 6 :y 2 :hit? false :ship? false}
+                                                     {:x 6 :y 3 :hit? false :ship? false}
+                                                     {:x 6 :y 4 :hit? false :ship? false}
+                                                     {:x 6 :y 5 :hit? false :ship? false}
+                                                     {:x 6 :y 6 :hit? false :ship? false}
+                                                     {:x 6 :y 7 :hit? false :ship? false}
+                                                     {:x 6 :y 8 :hit? false :ship? false}
+                                                     {:x 6 :y 9 :hit? false :ship? true}
+
+                                                     {:x 7 :y 0 :hit? false :ship? false}
+                                                     {:x 7 :y 1 :hit? false :ship? false}
+                                                     {:x 7 :y 2 :hit? false :ship? false}
+                                                     {:x 7 :y 3 :hit? false :ship? false}
+                                                     {:x 7 :y 4 :hit? false :ship? false}
+                                                     {:x 7 :y 5 :hit? false :ship? false}
+                                                     {:x 7 :y 6 :hit? false :ship? false}
+                                                     {:x 7 :y 7 :hit? false :ship? false}
+                                                     {:x 7 :y 8 :hit? false :ship? false}
+                                                     {:x 7 :y 9 :hit? false :ship? true}
+
+                                                     {:x 8 :y 0 :hit? false :ship? false}
+                                                     {:x 8 :y 1 :hit? false :ship? false}
+                                                     {:x 8 :y 2 :hit? false :ship? false}
+                                                     {:x 8 :y 3 :hit? false :ship? false}
+                                                     {:x 8 :y 4 :hit? false :ship? false}
+                                                     {:x 8 :y 5 :hit? false :ship? false}
+                                                     {:x 8 :y 6 :hit? false :ship? false}
+                                                     {:x 8 :y 7 :hit? false :ship? false}
+                                                     {:x 8 :y 8 :hit? false :ship? false}
+                                                     {:x 8 :y 9 :hit? false :ship? true}
+
+                                                     {:x 9 :y 0 :hit? false :ship? false}
+                                                     {:x 9 :y 1 :hit? false :ship? false}
+                                                     {:x 9 :y 2 :hit? false :ship? false}
+                                                     {:x 9 :y 3 :hit? false :ship? false}
+                                                     {:x 9 :y 4 :hit? false :ship? false}
+                                                     {:x 9 :y 5 :hit? false :ship? false}
+                                                     {:x 9 :y 6 :hit? false :ship? false}
+                                                     {:x 9 :y 7 :hit? false :ship? false}
+                                                     {:x 9 :y 8 :hit? false :ship? false}
+                                                     {:x 9 :y 9 :hit? false :ship? false}]}}}))
 
 (defn main [] window)
