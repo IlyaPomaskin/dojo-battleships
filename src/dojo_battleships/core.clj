840aaffc0178c051650896e3fa0c0eec867e229f
Oleg Tsymbalyuk <aberrant.hacker@gmail.com>
2019-03-24 16:16:41 +0300

    setup closure2d

--- src/dojo_battleships/core.clj
+++ src/dojo_battleships/core.clj
@@ -1,7 +1,28 @@
 (ns dojo-battleships.core
-  (:gen-class))
+    (:require [clojure2d.core :refer :all])
+    (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def canvas-width  800)
+(def canvas-height 600)
+
+(defn draw
+  "Some function decription."
+  [canvas ;; canvas to draw on
+   window ;; window bound to function (for mouse movements)
+   ^long framecount ;; frame number
+   state] ;; state (if any)
+  () ; should return a new state
+  )
+
+;; create canvas, display window and draw on canvas via draw function (60 fps)
+;; show-window {:keys [canvas window-name w h fps draw-fn state draw-state setup hint refresher always-on-top? background]
+(def window (show-window {:canvas (canvas canvas-width canvas-height),
+                          :window-name "Battleships",
+                          :fps 25,
+                          :draw-fn draw,
+                          :setup (fn [canvas window] ())}) ; returns initial state
+  )
+
+(defn main []
+  window)
+
