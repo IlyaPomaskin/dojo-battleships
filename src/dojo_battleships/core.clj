6ec7e168892122f522fb294ab4ce191f4a9d2a37
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-03-24 16:52:48 +0300

    render cells

--- src/dojo_battleships/core.clj
+++ src/dojo_battleships/core.clj
@@ -2,24 +2,30 @@
   (:require [clojure2d.core :refer :all])
   (:gen-class))
 
-(def field-size 10)
+(def world-size 10)
+(def field-size 30)
 (def ships [5 4 4 3 3 2 2 2 1 1 1 1])
 (def canvas-width  800)
 (def canvas-height 600)
 
-
-(defn render-board [board] nil)
-
-(defn handle-click [event state] state)
+(defn render-board [canvas board]
+  (set-color canvas :white 160)
+  (mapv #(rect canvas
+               (* field-size (:x %1))
+               (* field-size (:y %1))
+               field-size
+               field-size
+               true)
+        board))
 
 (defn draw
-  "Some function decription."
-  [canvas ;; canvas to draw on
-   window ;; window bound to function (for mouse movements)
-   ^long framecount ;; frame number
-   state] ;; state (if any)
-  nil ; should return a new state
-  )
+  [canvas
+   window
+   ^long framecount
+   state]
+  (set-background canvas :lightblue 200)
+  (render-board canvas (get-in state [:boards (:current-player state)]))
+  state)
 
 ;; create canvas, display window and draw on canvas via draw function (60 fps)
 ;; show-window {:keys [canvas window-name w h fps draw-fn state draw-state setup hint refresher always-on-top? background]
@@ -31,7 +37,13 @@
                                    {:state :setup ; :game :game-over
                                     :setup-ships-left ships
                                     :current-player :player-1
-                                    :board [{:x 0 :y 0 :hit? false :ship? false}]})}))
+                                    :boards {:player-1 [{:x 0 :y 0 :hit? false :ship? false}
+                                                        {:x 1 :y 0 :hit? false :ship? false}
+                                                        {:x 0 :y 1 :hit? false :ship? false}
+                                                        {:x 1 :y 1 :hit? false :ship? false}]
+                                             :player-2 [{:x 0 :y 0 :hit? false :ship? false}
+                                                        {:x 1 :y 0 :hit? false :ship? false}
+                                                        {:x 4 :y 0 :hit? false :ship? false}]}})}))
 
 (defn main []
   window)
