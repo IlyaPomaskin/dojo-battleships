08c3cc089cc15b9c297496bdcc35540eaacd2c80
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-03-24 17:18:58 +0300

    render various cells

--- src/dojo_battleships/core.clj
+++ src/dojo_battleships/core.clj
@@ -8,14 +8,48 @@
 (def canvas-width  800)
 (def canvas-height 600)
 
+(defn render-hit [canvas cell]
+  (-> canvas
+      (set-color :white 160)
+      (ellipse (+ (* field-size (:x cell)) (/ field-size 2))
+               (+ (* field-size (:y cell)) (/ field-size 2))
+               (/ field-size 2)
+               (/ field-size 2))))
+(defn render-ship [canvas cell]
+  (-> canvas
+      (set-color :blue 100)
+      (rect (* field-size (:x cell))
+            (* field-size (:y cell))
+            field-size
+            field-size)))
+(defn render-hit-ship [canvas cell]
+  (-> canvas
+      (render-ship cell)
+      (set-color :red 160)
+      (ellipse (+ (* field-size (:x cell)) (/ field-size 2))
+               (+ (* field-size (:y cell)) (/ field-size 2))
+               (/ field-size 2)
+               (/ field-size 2))))
+(defn render-empty [canvas cell]
+  (-> canvas
+      (set-color :white 160)
+      (rect (* field-size (:x cell))
+            (* field-size (:y cell))
+            field-size
+            field-size
+            true)))
+
+(defn get-render-cell-fn [cell]
+  (cond
+    (and (:hit? cell) (:ship? cell)) render-hit-ship
+    (:hit? cell) render-hit
+    (:ship? cell) render-ship
+    :else render-empty))
+
 (defn render-board [canvas board]
-  (set-color canvas :white 160)
-  (mapv #(rect canvas
-               (* field-size (:x %1))
-               (* field-size (:y %1))
-               field-size
-               field-size
-               true)
+  (mapv (fn [cell]
+          (let [render-fn (get-render-cell-fn cell)]
+            (render-fn canvas cell)))
         board))
 
 (defn draw
@@ -38,9 +72,9 @@
                                     :setup-ships-left ships
                                     :current-player :player-1
                                     :boards {:player-1 [{:x 0 :y 0 :hit? false :ship? false}
-                                                        {:x 1 :y 0 :hit? false :ship? false}
-                                                        {:x 0 :y 1 :hit? false :ship? false}
-                                                        {:x 1 :y 1 :hit? false :ship? false}]
+                                                        {:x 1 :y 0 :hit? false :ship? true}
+                                                        {:x 0 :y 1 :hit? true :ship? false}
+                                                        {:x 1 :y 1 :hit? true :ship? true}]
                                              :player-2 [{:x 0 :y 0 :hit? false :ship? false}
                                                         {:x 1 :y 0 :hit? false :ship? false}
                                                         {:x 4 :y 0 :hit? false :ship? false}]}})}))
